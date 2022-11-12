b4995d44dea05492cf30dd117439fe030d795675
TarantelFaser <jonathan@boeckel.name>
2022-11-12 22:55:18 +0100

    basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'algorithm-vizualisation';
+
+  buttonClicked() {
+    alert("Dieser Button wurde gedrückt");
+  }
 }
