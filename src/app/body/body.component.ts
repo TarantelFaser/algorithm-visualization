694452acfe7378931559899dd012e4c2ba3e5780
Jonathan <80773518+TarantelFaser@users.noreply.github.com>
2023-04-30 17:04:25 +0200

    Improved Clicking Animations

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,31 +1,43 @@
 import {Component, ElementRef,} from '@angular/core';
 
+export enum cellTypes {
+  Unused ,
+  Start,
+  End,
+  Path,
+  Selected,
+  Highlighted,
+  Checked
+}
+
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.scss']
 })
-export class BodyComponent{
 
-  public cellsHorizontal: number[] | undefined;
-  public cellsVertical: number[] | undefined;
+export class BodyComponent{
   public cellSize : number | undefined;
+  public cells : cellTypes[][]| undefined = [];
+
   constructor(private el : ElementRef) {
   }
 
-  ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
+  ngOnInit() {
     let width = this.el.nativeElement!.clientWidth;
     let height = this.el.nativeElement!.clientHeight;
-
     this.cellSize = 25;
 
     let maxCellCountHorizontal : number = Math.round((width - 0.01*width) / this.cellSize);
     let maxCellCountVertical : number = Math.round((height - 0.01*height) / this.cellSize);
 
-    this.cellsHorizontal = Array(maxCellCountHorizontal).fill(0);
-    this.cellsVertical = Array(maxCellCountVertical).fill(0);
+    for (let i = 0; i < maxCellCountVertical; i++) {
+      this.cells!.push([])
+      for (let j = 0; j < maxCellCountHorizontal; j++) {
+        this.cells![i].push(cellTypes.Unused);
+      }
+    }
   }
+
+  protected readonly cellTypes = cellTypes;
 }
